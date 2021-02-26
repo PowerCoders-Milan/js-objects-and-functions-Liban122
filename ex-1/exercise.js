023059b0cd3f226ae45e77aa97f93367bff1a04d
liban <botanlee515@gmail.com>
2021-02-26 22:08:52 +0100

    Done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,14 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+function user(){
+    var noun = prompt('Enter a noun');
+    var verb = prompt('Enter a verb');
+    var adjective = prompt('Enter an adjective');
+    var adverb = prompt('Enter an adverb');
+    alert(noun  + ' ' + verb + ' ' + ' ' + adverb + ' '+ adjective)
+}
+
+user()
+

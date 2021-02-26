1c49d9e315365ae906424f61fe807ddbd16acfb3
liban <botanlee515@gmail.com>
2021-02-26 21:40:27 +0100

    Done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,14 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function tellFortune() {
+    var place = prompt("Where do you want to live?");
+    var futureJob = prompt("What would you like to become?");
+    var futurePartner = prompt("Who is your future partner? ");
+    var futureChildren = prompt("How many children would you like to have?"); 
+    window.alert("You will be a " + futureJob +" in " + place +", and married to " + futurePartner + " with " + futureChildren +" kids."); 
+}
+
+tellFortune();
+tellFortune();
+tellFortune();

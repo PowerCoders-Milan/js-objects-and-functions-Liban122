83be20e782350bf9b1be2e6910a8fb3bd160ca20
liban <botanlee515@gmail.com>
2021-02-26 22:28:14 +0100

    Done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,16 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+var myTopChoices = ['Iphone', 'black or siver colors' , 'medium Size phone']
+alert(myTopChoices[0] + ' is my 1st choice');
+alert(myTopChoices[1] + ' is my 2nd choice');
+alert(myTopChoices[2] + ' is my 3rd choice')
+
+var choices ={
+    phone: 'Iphone',
+    color: 'black or silver',
+    size: ' Medium'
+}
+alert(choices.phone);
+alert(choices.color);
+alert(choices.size);

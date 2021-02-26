057758e441723e89a93726e418bef68c0390c096
liban <botanlee515@gmail.com>
2021-02-27 00:44:45 +0100

    Done

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,3 +1,18 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function greaterNum(num1, num2){
+    if(num1> num2){
+       return alert(num1 + ' is greater than ' + num2)
+    }
+    else if(num2 > num1){
+        return alert (num2 + ' is greater than ' + num1)
+    }
+    else{
+        alert('Both Numbers are equal')
+    }
+}
+
+greaterNum(6, 2)
+greaterNum(4,9)
+greaterNum(3, 3)
